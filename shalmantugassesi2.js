bf6358c26ae1dbfc53faa44ec0e63f8d0846bc90
Muhamadshalman18 <147075684+Muhamadshalman18@users.noreply.github.com>
2023-10-05 22:15:46 +0700

    Add files via upload

---
+++ shalmantugassesi2.js
@@ -0,0 +1,13 @@
+var fullName = "Muhamad"
+var lastName = "Shalman"
+var asalKampus = "Nusa Putra"
+var asalProdi = "Program Studi Teknik Informatika"
+var age = 19;
+var isMarried = false;
+
+console.log(fullName);
+console.log(lastName);
+console.log(asalKampus);
+console.log(asalProdi);
+console.log ("age:",age);
+console.log("isMarried:", isMarried);
